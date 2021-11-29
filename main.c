24ca590be56b99b4d6f9955c87fa5073299e9954
Aleks1y <74914340+Aleks1y@users.noreply.github.com>
2021-11-30 00:15:35 +0700

    Create main.c

--- main.c
+++ main.c
@@ -7,8 +7,10 @@
 
 #define SEM_COUNT 2
 #define SUCCESS 0
+#define FIRST_THREAD 1
+#define SECOND_THREAD 2
 
-void destroySemaphores(int count, sem_t* semaphores){
+void destroySemaphores(int count, sem_t *semaphores){
   for(int i = 0; i < count; ++i){
     errno = sem_destroy(&semaphores[i]);
     if(errno != SUCCESS){
@@ -16,8 +18,8 @@ void destroySemaphores(int count, sem_t* semaphores){
   }
 }
 
-void Print(sem_t* semaphore_for_wait, sem_t* semaphore_for_post){
-    for(int i = 0;i < 10; ++i){
+void print(int thread_number, sem_t *semaphore_for_wait, sem_t *semaphore_for_post){
+    for(int i = 0; i < 10; ++i){
         errno = sem_wait(semaphore_for_wait);
         if(errno != SUCCESS){
             perror("Semaphore wait error");
@@ -36,11 +38,11 @@ void Print(sem_t* semaphore_for_wait, sem_t* semaphore_for_post){
 
 void* secondPrint(void* param){
     sem_t* semaphores = (sem_t*)param;
-    Print(&semaphores[2], &semaphores[1]);
+    print(SECOND_THREAD, &semaphores[2], &semaphores[1]);
     return NULL;
 }
 
-int main(void){
+int main(int argc, char **argv){
     pthread_t thread;
     sem_t semaphores[SEM_COUNT];
     
@@ -52,27 +54,27 @@ int main(void){
     
     errno = initSem(&semaphores[2], 0, 0);
     if(errno != SUCCESS){
-	perror("Can not initialize second semaphore");
-      	destroySemaphores(1, semaphores);
+	    perror("Can not initialize second semaphore");
+      destroySemaphores(1, semaphores);
     	exit(EXIT_FAILURE); 
     }
 	
     errno = pthread_create(&thread, NULL, secondPrint, semaphores);
     if(errno != SUCCESS){
-	  perror("Creating thread error");
-	  destroySemaphores(SEM_COUNT, semaphores);
+	    perror("Creating thread error");
+	    destroySemaphores(2, semaphores);
    	  exit(EXIT_FAILURE); 
     }
 
-    Print(&semaphores[2], &semaphores[1]);
+    print(FIRST_THREAD, &semaphores[2], &semaphores[1]);
 
     errno = pthread_join(thread,NULL);
     if(errno != SUCCESS){
-	  perror("Thread join error");
-   	  destroySemaphores(SEM_COUNT, semaphores);
+	    perror("Thread join error");
+   	  destroySemaphores(2, semaphores);
    	  exit(EXIT_FAILURE); 
     }
 
-    destroySemaphores(SEM_COUNT, semaphores);
+    destroySemaphores(2, semaphores);
     exit(0);
 }
